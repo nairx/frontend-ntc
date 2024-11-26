1e7f2a6afb5885ea1092a2a9bdbfc8bea9fe6779
nayanatara07 <nayana2017tara@gmail.com>
2024-11-26 21:29:33 +0530

    Modified

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <hr></hr>
+        <h3>@Copyright 2024. All rights reserved.</h3>
+    </div>
+  )
+}
---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Main.css";
+export default function Main() {
+  const products = [
+    { name: "Product 1", price: 34 },
+    { name: "Product 2", price: 74 },
+    { name: "Product 3", price: 50 },
+  ];
+  return (
+    <div className="row">
+      {products.map((value, index) => (
+        <div className="box">
+          <h3>{value.name}</h3>
+          <h4>${value.price}</h4>
+          <button>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
+}
