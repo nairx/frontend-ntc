13135a8f18c0bfcf99bea1961734a264ec436479
nairx <nairxprv@gmail.com>
2024-12-05 21:30:08 +0530

    added

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const [email,setEmail] = useState()
+  const [pass,setPass] = useState()
   const Navigate = useNavigate();
   const handleSubmit = () => {
     Navigate("/signup");
   };
+  const url = "http://localhost:8080/find"
+  const handleLogin = async () => {
+    const result = await axios.post(url,{email,pass})
+  }
   return (
     <div>
       Login
       <p>
-        <input type="text" placeholder="Enter Email"></input>
+        <input type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"></input>
       </p>
       <p>
-        <input type="password" placeholder="Enter Password"></input>
+        <input type="password" onChange={(e)=>setPass(e.target.value)} placeholder="Enter Password"></input>
       </p>
-      <button>Login</button>
+      <button onClick={handleLogin}>Login</button>
       <hr></hr>
       <button onClick={handleSubmit}>Create Account</button>
     </div>
--- src/Signup.js
+++ src/Signup.js
@@ -1,23 +1,32 @@
 import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from 'axios'
 export default function Signup() {
+  const [name,setName] = useState()
+  const [pass,setPass] = useState()
+  const [email,setEmail] = useState()
     const Navigate = useNavigate()
     const handleSubmit = () => {
         Navigate("/login")
+    }
+    const url = "http://localhost:8080/users"
+    const handleSignup = async () => {
+      const result = await axios.post(url,{name:name,pass:pass,email:email})
     }
   return (
     <div>
       Signup
       <p>
-        <input type="text" placeholder="Enter Name"></input>
+        <input type="text" onChange={(e)=>setName(e.target.value)} placeholder="Enter Name"></input>
       </p>
       <p>
-        <input type="text" placeholder="Enter Email"></input>
+        <input type="text" onChange={(e)=>setEmail(e.target.value)} placeholder="Enter Email"></input>
       </p>
       <p>
-      <input type="password" placeholder="Enter Password"></input>
+      <input type="password" onChange={(e)=>setPass(e.target.value)} placeholder="Enter Password"></input>
       </p>
-      <button>Signup</button>
+      <button onClick={handleSignup}>Signup</button>
       <hr></hr>
       <button onClick={handleSubmit}>Already a member? Login Here...</button>
     </div>
